9e46fb8168d8d85bda6f1e4fd9b6d2bd4c4e7aaf
Geetha <geetha@example.com>
2025-12-29 23:14:46 +0530

    Initial commit - Chatbot-GenAI

---
+++ main.py
@@ -0,0 +1,21 @@
+from google import genai
+from flask import Flask, request, jsonify, render_template
+client = genai.Client(api_key="AIzaSyAOmDoOXV79A_b04f2o52pmUA4MgfxCYNA")
+
+
+app = Flask(__name__, template_folder="templates")
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    prompt = request.json["message"]
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
+    return jsonify({"reply": response.text})
+
+app.run(port="8000")
